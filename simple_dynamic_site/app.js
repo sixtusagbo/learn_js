22cb90cce276210deda7c23d0df436c9a725d0dc
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-16 18:03:58 +0100

    Add user route and compartmentalize my code

--- simple_dynamic_site/app.js
+++ simple_dynamic_site/app.js
@@ -1,31 +1,12 @@
+let router = require("./router");
+
 // Create a web server
 let http = require("http");
 http.createServer(function (request, response) {
-  homeRoute(request, response);
+  router.home(request, response);
+  router.user(request, response);
 }).listen(1337, "127.0.0.1");
 console.log("Server running at http://127.0.0.1:1337/");
-
-// Handle HTTP route GET / and POST /
-function homeRoute(request, response) {
-  // if url == "/" && GET
-  if (request.url === "/") {
-    // show search
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Header\n");
-    response.write("Search\n");
-    response.end("Footer\n");
-  }
-  // if url == "/" && POST
-  // redirect to /:username
-}
-
-// Handle HTTP route GET /:username and POST /
-  // if url == "/..."
-    // get json
-      // on "end"
-        // show profile
-      // on "error"
-        // show error
 
 // Read files and merge value
   // read from file and get a string
---
+++ simple_dynamic_site/router.js
@@ -0,0 +1,33 @@
+// Handle HTTP route GET / and POST /
+function home(request, response) {
+  // if url == "/" && GET
+  if (request.url === "/") {
+    // show search
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("Footer\n");
+  }
+  // if url == "/" && POST
+  // redirect to /:username
+}
+
+// Handle HTTP route GET /:username and POST /
+function user(request, response) {
+  // if url == "/..."
+  let username = request.url.replace("/", "");
+  if (username.length > 0) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Header\n");
+    response.write(username + "\n");
+    response.end("Footer\n");
+  }
+  // get json
+  // on "end"
+  // show profile
+  // on "error"
+  // show error
+}
+
+module.exports.home = home;
+module.exports.user = user;
