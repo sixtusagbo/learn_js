86bf57a8ff469e25138d1f9fb155983f0ae26f18
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-16 17:50:08 +0100

    Handle home route

--- simple_dynamic_site/app.js
+++ simple_dynamic_site/app.js
@@ -1,18 +1,23 @@
 // Create a web server
 let http = require("http");
 http.createServer(function (request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Before end\n");
-  setInterval(() => response.write(new Date + "\n"), 1000);
-  // response.end("Hello World\n");
+  homeRoute(request, response);
 }).listen(1337, "127.0.0.1");
 console.log("Server running at http://127.0.0.1:1337/");
 
 // Handle HTTP route GET / and POST /
+function homeRoute(request, response) {
   // if url == "/" && GET
+  if (request.url === "/") {
     // show search
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("Footer\n");
+  }
   // if url == "/" && POST
-    // redirect to /:username
+  // redirect to /:username
+}
 
 // Handle HTTP route GET /:username and POST /
   // if url == "/..."
