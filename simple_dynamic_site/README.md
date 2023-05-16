be1a99bc64cb37e117947c7446d7b21d0fce2318
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-16 19:51:47 +0100

    Switch github api

--- simple_dynamic_site/README.md
+++ simple_dynamic_site/README.md
@@ -1,5 +1,2 @@
 # Simple Dynamic Site
-Simple way to check out a user's profile. I use Node.js to look up profile through am api (Currently [TreeHouse](https://teamtreehouse.com) API) an serve it to the browser.
-
-## Future improvements
-- Use [GitHub](https://github.com) API instead
+Simple way to check out a user's profile. I use Node.js to look up profile through [GitHub](https://github.com) API and serve it to the browser.
--- simple_dynamic_site/router.js
+++ simple_dynamic_site/router.js
@@ -13,20 +13,42 @@ function home(request, response) {
 }
 
 // Handle HTTP route GET /:username and POST /
-function user(request, response) {
+async function user(request, response) {
   // if url == "/..."
   let username = request.url.replace("/", "");
   if (username.length > 0) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.write("Header\n");
-    response.write(username + "\n");
-    response.end("Footer\n");
+
+    await fetch(`https://api.github.com/users/${username}`)
+      .then(async data => {
+        if (data.status === 200) {
+
+          let profile = await data.json();
+
+          // store the values we need
+          let values = {
+            avatarUrl: profile.avatar_url,
+            username: profile.login,
+            repos: profile.public_repos,
+            followers: profile.followers,
+          }
+
+          // show profile
+          response.write(values.username + " has " + values.repos + " repositories.\n");
+          response.end("Footer\n");
+        } else {
+          // show error
+          response.write(data.status + " " + data.statusText + "\n");
+          response.end("Footer\n");
+        }
+      })
+      .catch(error => {
+        // show error
+        response.write(error.message + "\n");
+        response.end("Footer\n");
+      });
   }
-  // get json
-  // on "end"
-  // show profile
-  // on "error"
-  // show error
 }
 
 module.exports.home = home;
